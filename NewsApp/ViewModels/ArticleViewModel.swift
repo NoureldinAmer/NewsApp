061e21526f466f6444f1d838dca2507829e740c4
Nour Amer <84950398+NoureldinAmer@users.noreply.github.com>
2022-06-17 21:58:45 +0400

    implemented article view model

--- NewsApp/ViewModels/ArticleViewModel.swift
+++ NewsApp/ViewModels/ArticleViewModel.swift
@@ -4,3 +4,21 @@
 //
 //  Created by Noureldin Amer on 2022-06-09.
 //
+
+import SwiftUI
+
+class ArticleListViewModel : ObservableObject {
+    @Published var articles : [Article]
+    
+    var newsAPI = NewsAPI.shared
+    
+    init() {
+        self.articles = []
+    }
+    
+    // MARK: - Intent(s)
+    func fetch(category: Category) async throws -> Void {
+        self.articles = try! await newsAPI.fetch(from: category)
+    }
+    
+}
